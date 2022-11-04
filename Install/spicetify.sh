520dd0df769256ba6dee8c1e3d526bc39dac6a9d
Ladislus <walcak.ladislas@gmail.com>
2022-11-04 16:35:49 +0100

    [MOD] Updated spicetify install script to newest commands

--- Install/spicetify.sh
+++ Install/spicetify.sh
@@ -6,6 +6,6 @@ git clone https://github.com/morpheusthewhite/spicetify-themes
 mv -r spicetify-themes/* .
 rm -rf spicetify-themes LICENSE README.md CODE_OF_CONDUCT.md .gitignore
 # Mise à jour de Spotify
-spicetify backup apply enable-devtool
+spicetify backup apply
 spicetify config current_theme Onepunch
-spicetify apply disable-devtool
+spicetify apply

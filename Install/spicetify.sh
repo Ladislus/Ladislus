e8a4cfc5253f85fb444a037b2339ae9c89625fd9
Ladislus <walcak.ladislas@gmail.com>
2022-11-04 16:57:04 +0100

    [MOD] Updated spicetify script

--- Install/spicetify.sh
+++ Install/spicetify.sh
@@ -3,9 +3,9 @@ sudo chmod a+wr /opt/spotify -R
 # Téléchargement des themes
 cd ~/.config/spicetify/Themes/
 git clone https://github.com/morpheusthewhite/spicetify-themes
-mv -r spicetify-themes/* .
-rm -rf spicetify-themes LICENSE README.md CODE_OF_CONDUCT.md .gitignore
+mv spicetify-themes/* .
+rm -rf spicetify-themes LICENSE README.md CODE_OF_CONDUCT.md CONTRIBUTING.md THEMES.md manifest.json .gitignore
 # Mise à jour de Spotify
 spicetify backup apply
-spicetify config current_theme Onepunch
+spicetify config current_theme Ziro
 spicetify apply

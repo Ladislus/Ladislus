86c11436b08ad5a6f5ec1b035f2f99444417c1b0
Ladislus <walcak.ladislas@gmail.com>
2023-02-13 22:15:17 +0100

    [MOD] Moved spicetify part to external script, as it must be launched after spotify first launch

--- Install/installer.sh
+++ Install/installer.sh
@@ -88,23 +88,6 @@ rustup default stable
 rustup -V
 
 #####################
-#     SPICETIFY     #
-#####################
-
-# Set permissions to change spotify look
-sudo chmod a+wr /opt/spotify -R
-
-# Download themes
-git -C ~/.config/spicetify/Themes/ clone https://github.com/morpheusthewhite/spicetify-themes
-mv ~/.config/spicetify/Themes/spicetify-themes/* ~/.config/spicetify/Themes/
-rm -rf ~/.config/spicetify/Themes/spicetify-themes
-
-# Set Spotify theme
-spicetify backup apply
-spicetify config current_theme BurntSienna
-spicetify apply
-
-#####################
 #     RUSTYVIBES    #
 #####################
 
